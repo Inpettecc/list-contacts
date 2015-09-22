c1a15799e489a25c6b90abadb6b7f752fd227d70
danielpsf <danielpsf@gmail.com>
2015-09-22 20:12:41 -0300

    Adding contatos_mock.js; changing index.html (inserting comments); removing unused logo

---
+++ www/js/contatos_mock.js
@@ -0,0 +1,102 @@
+var contatosMock = [
+	{
+		"id": "",
+		"displayName": "",
+		"name": {
+			"formatted": "",
+			"familyName": "",
+			"givenName": "",
+			"middleName": "",
+			"honorificPrefix": "",
+			"honorificSuffix": ""
+		},
+		"nickname": "",
+		"phoneNumbers": [
+			{
+				"type": "",
+				"value": "",
+				"pref": true
+			},
+			{
+				"type": "",
+				"value": "",
+				"pref": true
+			}
+		],
+		"emails": [
+			{
+				"type": "",
+				"value": "",
+				"pref": true
+			}
+		],
+		"addresses": [
+			{
+				"pref": true,
+				"type": "",
+				"formatted": "",
+				"streetAddress": "",
+				"locality": "",
+				"region": "",
+				"postalCode": "",
+				"country": ""
+			},
+			{
+				"pref": true,
+				"type": "",
+				"formatted": "",
+				"streetAddress": "",
+				"locality": "",
+				"region": "",
+				"postalCode": "",
+				"country": ""
+			}
+		],
+		"ims": [
+			{
+				"type": "",
+				"value": "",
+				"pref": true
+			}
+		],
+		"organizations": [
+			{
+				"pref": true,
+				"type": "",
+				"name": "",
+				"department": "",
+				"title": ""
+			},
+			{
+				"pref": true,
+				"type": "",
+				"name": "",
+				"department": "",
+				"title": ""
+			}
+		],
+		"birthday": "Tue Sep 01 2015 14:37:07 GMT-0300 (Hora oficial do Brasil)" // new Date()
+		"note": "",
+		"photos": [
+			{
+				"type": "",
+				"value": "",
+				"pref": true
+			}
+		],
+		"categories": [
+			{
+				"type": "",
+				"value": "",
+				"pref": true
+			}
+		],
+		"urls":[
+			{
+				"type": "",
+				"value": "",
+				"pref": true
+			}
+		]
+	}
+]
